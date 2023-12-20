0476f5a493e72720566b7d082886a23276b75f01
Pradeepto Sarkar <psrcofficial10@gmail.com>
2023-12-20 17:38:23 +0530

    series of roots

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button'
 export default function Home() {
   return (
     <main>
-      <Button variant="destructive" className="">Hello</Button>
+      
     </main>
   )
 }
