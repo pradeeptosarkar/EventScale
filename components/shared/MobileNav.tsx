3ec8801f6ab0c53a0512322e79854580f94074ea
Pradeepto Sarkar <psrcofficial10@gmail.com>
2023-12-22 11:06:39 +0530

    series of roots 2.0

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -6,6 +6,7 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import { Separator } from "@radix-ui/react-separator";
 import Image from "next/image";
 
 const MobileNav = () => {
@@ -13,22 +14,22 @@ const MobileNav = () => {
     <nav className="md:hidden">
       <Sheet>
         <SheetTrigger className="align-middle">
-          <Image 
-          src='/assets/icons/menu.svg'
-          alt="menu"
-          width={24}
-          height={24}
-          className="cursor-pointer"
+          <Image
+            src="/assets/icons/menu.svg"
+            alt="menu"
+            width={24}
+            height={24}
+            className="cursor-pointer"
           />
         </SheetTrigger>
-        <SheetContent>
-          <SheetHeader>
-            <SheetTitle>Are you sure absolutely sure?</SheetTitle>
-            <SheetDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </SheetDescription>
-          </SheetHeader>
+        <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={128}
+            height={38}
+          />
+          <Separator className="border border-gray-50" />
         </SheetContent>
       </Sheet>
     </nav>
