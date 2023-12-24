5a5af0f07a7c1551569eff1d446fab684eddbbdc
Pradeepto Sarkar <psrcofficial10@gmail.com>
2023-12-24 17:57:54 +0530

    final version

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,18 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-    publicRoutes: [
-        '/',
-        '/events/id',
-        '/api/webhook/clerk',
-        '/api/webhook/stripe',
-        '/api/uploadthing'
-    ],
-
-    ignoredRoutes: [
-        '/api/webhook/clerk',
-        '/api/webhook/stripe',
-        '/api/uploadthing'
-    ]
+  publicRoutes: [
+    '/',
+    '/events/:id',
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ],
+  ignoredRoutes: [
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ]
 });
  
 export const config = {
