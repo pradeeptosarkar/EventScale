93e537345d2265120ce11ad2eef9725862182e3a
Pradeepto Sarkar <psrcofficial10@gmail.com>
2023-12-26 21:49:22 +0530

    landing page update

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,23 +1,41 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className="border-t">
-      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
-        <Link href='/'>
-          <Image 
-            src="/assets/images/logo(2).png"
-            alt="logo"
-            width={128}
-            height={38}
-          />
-        </Link>
+  <div className="wrapper p-5">
+    {/* First Section - Logo */}
+    <div className="flex justify-center text-center mb-4">
+      <Link href="/">
+        <Image
+          src="/assets/images/logo(2).png"
+          alt="logo"
+          width={200}
+          height={60}
+        />
+      </Link>
+    </div>
 
-        <p>2023 eventscale. All Rights reserved.</p>
-      </div>
-    </footer>
-  )
-}
+    {/* Second Section - Remaining Components */}
+    <div className="flex flex-col lg:flex-row justify-between items-center">
+      <p>
+        Made with ❤️ by{" "}
+        <a
+          href="https://github.com/pradeeptosarkar"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="cursor-pointer text-indigo-600 hover:underline"
+        >
+          Pradeepto Sarkar
+        </a>
+      </p>
+      <p>2023 eventscale. All Rights reserved.</p>
+    </div>
+  </div>
+</footer>
 
-export default Footer
+  );
+};
+
+export default Footer;
